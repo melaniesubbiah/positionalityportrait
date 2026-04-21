40d0cbebe7194b9d4d3c978f5281315b42a9f1fb
Melanie Sangita Subbiah <melanie.s.subbiah@gmail.com>
2026-04-21 19:57:49 -0400

    odds and ends

--- generate_portrait.sh
+++ generate_portrait.sh
@@ -9,8 +9,8 @@ python scripts/score_emotions.py --input_dir summaries --model $1
 echo "Scoring themes..."
 python scripts/score_themes.py --input_dir summaries --model $1
 
-echo "Generating full portrait for model: $1"
-python scripts/positionality_portrait.py --model_path $1
+#echo "Generating full portrait for model: $1"
+#python scripts/positionality_portrait.py --model_path $1
 
 echo "Generating subset portrait for model: $1"
 python scripts/positionality_portrait.py --model_path $1 --attribute_subset
