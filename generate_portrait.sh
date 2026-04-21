e065f78bb5c724742bf054d628b8c78b524f5ef1
Melanie Sangita Subbiah <melanie.s.subbiah@gmail.com>
2026-04-21 18:37:59 -0400

    add shell script

---
+++ generate_portrait.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Scoring semantics..."
+python scripts/score_semantics.py --input_dir summaries --model $1
+
+echo "Scoring emotions..."
+python scripts/score_emotions.py --input_dir summaries --model $1
+
+echo "Scoring themes..."
+python scripts/score_themes.py --input_dir summaries --model $1
+
+echo "Generating full portrait for model: $1"
+python scripts/positionality_portrait.py --model_path $1
+
+echo "Generating subset portrait for model: $1"
+python scripts/positionality_portrait.py --model_path $1 --attribute_subset
